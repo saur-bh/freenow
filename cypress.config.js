c14cf717d59e93795235fd4927ffbce0e371aef0
saur-bh <saurabh@enjin.io>
2022-09-07 18:08:43 +0530

    fixed reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,26 @@
 const { defineConfig } = require('cypress');
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = defineConfig({
+	reporter: 'cypress-mochawesome-reporter',
+	reporterOptions: {
+		charts: true,
+		reportPageTitle: 'Automation Test Resport for Gerimedica',
+		embeddedScreenshots: true,
+		inlineAssets: true,
+		saveAllAttempts: false,
+	},
 	e2e: {
 		setupNodeEvents(on, config) {
-			// implement node event listeners here
+			on('before:run', async details => {
+				console.log('override before:run');
+				await beforeRunHook(details);
+			});
+
+			on('after:run', async () => {
+				console.log('override after:run');
+				await afterRunHook();
+			});
 		},
 	},
 });
